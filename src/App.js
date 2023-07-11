87dae5bce3bd1f349dd1d9a84edb35a47b6453a4
Jasontlg <jasont@theleatherguy.org>
2023-07-11 10:20:29 -0400

    Updated file per lab instructions

--- src/App.js
+++ src/App.js
@@ -6,26 +6,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
-
+import Remaining from './components/Remaining';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
+import ExpenseTotal from './components/ExpenseTotal';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
 
-                        {/* Add Remaining component here under */}        
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div> 
 
-                        {/* Add ExpenseItem component here under */}        
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                        </div>
+                        <ExpenseList />
 
-                        {/* Add AllocationForm component here under */}        
+                    <h3 classname='mt-3'>Change Allocation</h3>
+                    <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                        </div></div>
 
                 </div>
             </div>
